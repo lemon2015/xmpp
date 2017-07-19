7eea109cdec2d9565a19487b3cebea74d05f092a
lemon2015 <332778006@qq.com>
2017-07-19 14:31:58 +0800

    --wxj

--- test.php
+++ test.php
@@ -1,21 +1,21 @@
 <?php
-//$arr = array(
-//    'localuser'=>'mark',
-//    'localserver'=>'mark',
-//    'user'=>'lemon',
-//    'server'=>'lemon',
-//    'nick'=>'hehe',
-//    'group'=>'servicer',
-//    'subs'=>'haha'
-//);
+$arr = array(
+    'localuser'=>'mark',
+    'localserver'=>'mark',
+    'user'=>'lemon',
+    'server'=>'lemon',
+    'nick'=>'hehe',
+    'group'=>'servicer',
+    'subs'=>'haha'
+);
 
 $arr = array(
     'host'=>'127.0.0.1',
     'user'=>'mark'
 );
 
-//$arr = array(
-//    'name'=>"mark"
-//);
+$arr = array(
+    'name'=>"mark"
+);
 
 echo json_encode($arr);
